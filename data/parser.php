baa788f9e1c79ece8e8196cc66ab264914ae3704
Filip Procházka <filip@prochazka.su>
2015-05-19 18:38:05 +0200

    Created ElearningPreviewParser

--- data/parser.php
+++ data/parser.php
@@ -1,5 +1,13 @@
 <?php
 
+use Atrox\Matcher;
+
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+
+
 class QdefParser
 {
 
@@ -9,6 +17,13 @@ class QdefParser
 	public function __construct($contents)
 	{
 		$this->questions = $this->toQuestions($this->toBlocks($contents));
+	}
+
+
+
+	public function toArray()
+	{
+		return $this->questions;
 	}
 
 
@@ -83,9 +98,59 @@ class QdefParser
 
 
 
+	/**
+	 * @param string $file
+	 * @return QdefParser
+	 */
+	public static function fromFile($file)
+	{
+		return new static(file_get_contents($file));
+	}
+
+}
+
+
+class ElearningPreviewParser
+{
+
+	private $questions = [];
+
+
+
+	public function __construct($contents)
+	{
+		$this->questions = $this->toQuestions($contents);
+	}
+
+
+
 	public function toArray()
 	{
 		return $this->questions;
+	}
+
+
+
+	private function toQuestions($contents)
+	{
+		$matcher = $this->buildMatcher();
+		return $matcher($contents);
+	}
+
+
+
+	protected function buildMatcher()
+	{
+		return Matcher::multi('//div[@id="aplikace"]/form/table[not(@bgcolor)]//tr/td[position() = 3]', [
+			'name' => './/text()[2]',
+			'answers' => Matcher::multi('./label', [
+				'body' => './text()[last()]',
+				'right' => function (DOMElement $node) {
+					$asterisk = (new DOMXPath($node->ownerDocument))->query('.//font[@color="green"]/text()', $node);
+					return $asterisk->length > 0 && trim($asterisk->item(0)->wholeText) === '*';
+				}
+			]),
+		])->fromHtml();
 	}
 
 
@@ -136,3 +201,9 @@ class JsonQuestions
 //	->addQuestions(QdefParser::fromFile(__DIR__ . '/PV004/pristupova_prava_ls_oct.qdef')->toArray())
 //	->addQuestions(QdefParser::fromFile(__DIR__ . '/PV004/pristupova_prava_oct_ls.qdef')->toArray())
 	->write(__DIR__ . '/../questions_PV004.json');
+
+$vb036 = (new JsonQuestions());
+foreach (glob(__DIR__ . '/vb036/*.html') as $file) {
+	$vb036->addQuestions(ElearningPreviewParser::fromFile($file)->toArray());
+}
+$vb036->write(__DIR__ . '/../questions_VB036.json');
