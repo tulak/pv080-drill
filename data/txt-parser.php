99f46ec093885d415665d91b92bae2d687ca59af
Filip Procházka <filip@prochazka.su>
2016-01-12 16:09:35 +0100

    add source file for PV065

--- data/txt-parser.php
+++ data/txt-parser.php
@@ -8,5 +8,5 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 
 (new JsonQuestions())
-    ->addQuestions(TxtParser::fromFile(__DIR__ . '/PV065-vypracovane-otazky-verze-19-1-2015-pv065-unix.txt')->toArray())
+    ->addQuestions(TxtParser::fromFile(__DIR__ . '/data-PV065-vypracovane-otazky-verze-19-1-2015-pv065-unix.txt')->toArray())
     ->write(__DIR__ . '/../datasets/questions_PV065.json');
